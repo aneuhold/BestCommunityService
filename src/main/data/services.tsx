1e829b3c0c84cb1b23f4af3c6756a55c28eee9b5
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-06 13:09:26 -0800

    I#1 Add services.tsx

---
+++ src/main/data/services.tsx
@@ -0,0 +1,27 @@
+type ServiceType = 'inHouseService' | 'externalService';
+
+interface Service {
+  type: ServiceType;
+  name: string;
+  displayName: string;
+  description: string;
+}
+
+type Services = Service[];
+
+const services: Services = [
+  {
+    name: 'catering',
+    displayName: 'Catering',
+    type: 'externalService',
+    description: 'Catering for all your events. Big or small.',
+  },
+  {
+    name: 'homeCare',
+    displayName: 'Home Care',
+    type: 'externalService',
+    description: 'In-home care for elderly, and handicapped.',
+  },
+];
+
+export default services;
