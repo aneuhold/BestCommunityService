1c0260d10d25027665673eda3bc106b1b1ffdf89
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-20 17:02:20 -0800

    I #10 Add content to the about page

--- src/main/routes/About.tsx
+++ src/main/routes/About.tsx
@@ -1,11 +1,43 @@
+import { makeStyles, Theme, createStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import { LoremIpsum } from 'lorem-ipsum';
 
-const About = (): JSX.Element => {
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+});
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      textAlign: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      backgroundColor: theme.palette.background.default,
+    },
+    bodyText: {
+      maxWidth: '700px',
+    },
+  })
+);
+
+export default function About(): JSX.Element {
+  const classes = useStyles();
   return (
-    <>
-      <div>This is an about page</div>
-    </>
+    <div className={classes.root}>
+      <Typography variant="h2" component="h1">
+        About Us
+      </Typography>
+      <Typography variant="body1" component="p" className={classes.bodyText}>
+        {lorem.generateParagraphs(1)}
+      </Typography>
+      <br />
+      <Typography variant="body1" component="p" className={classes.bodyText}>
+        {lorem.generateParagraphs(1)}
+      </Typography>
+    </div>
   );
-};
-
-export default About;
+}
