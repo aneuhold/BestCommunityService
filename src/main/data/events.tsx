6ac147c571e981e5012d662ef3201ecce84c5a2b
Anton G Neuhold Jr <agneuhold@gmail.com>
2021-02-20 20:01:05 -0800

    I #11 Add a calendar page

--- src/main/data/events.tsx
+++ src/main/data/events.tsx
@@ -58,3 +58,56 @@ export const scheduledEvents: Array<ScheduledEvent> = [
     dateRange: [new Date(2021, 1, 13, 14), new Date(2021, 1, 13, 15)],
   },
 ];
+
+export const exampleCalendarEvents = [
+  {
+    title: 'DnD Night',
+    startDate: new Date(2018, 6, 23, 9, 30),
+    endDate: new Date(2018, 6, 23, 11, 30),
+  },
+  {
+    title: 'College Info Session',
+    startDate: new Date(2018, 6, 23, 12, 0),
+    endDate: new Date(2018, 6, 23, 13, 0),
+  },
+  {
+    title: 'Community Organizer Meeting',
+    startDate: new Date(2018, 6, 23, 14, 30),
+    endDate: new Date(2018, 6, 23, 15, 30),
+  },
+  {
+    title: 'Boy Scout Meeting - Troop 404',
+    startDate: new Date(2018, 6, 24, 10, 0),
+    endDate: new Date(2018, 6, 24, 11, 0),
+  },
+  {
+    title: 'Why are parrots multi-color? An info session.',
+    startDate: new Date(2018, 6, 24, 12, 0),
+    endDate: new Date(2018, 6, 24, 13, 35),
+  },
+  {
+    title: 'Python Bootcamp',
+    startDate: new Date(2018, 6, 27, 16, 30),
+    endDate: new Date(2018, 6, 27, 18, 0),
+  },
+  {
+    title: 'Hiking Preparation Workshop',
+    startDate: new Date(2018, 6, 16, 9, 30),
+    endDate: new Date(2018, 6, 16, 15, 30),
+  },
+  {
+    title: 'How to build a Dyson Sphere',
+    startDate: new Date(2018, 6, 20, 14, 10),
+    endDate: new Date(2018, 6, 20, 15, 30),
+  },
+  {
+    title: 'Art Class',
+    startDate: new Date(2018, 5, 22),
+    endDate: new Date(2018, 6, 1),
+  },
+  {
+    title: 'Computer Class',
+    startDate: new Date(2018, 6, 28),
+    endDate: new Date(2018, 7, 7),
+  },
+];
--- src/main/routes/Calendar.tsx
+++ src/main/routes/Calendar.tsx
@@ -1,7 +1,42 @@
+import { makeStyles, Theme, createStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import {
+  Scheduler,
+  MonthView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import { exampleCalendarEvents } from '../data/events';
 
-const Calendar = (): JSX.Element => {
-  return <>This will be something someday!</>;
-};
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      textAlign: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      backgroundColor: theme.palette.background.default,
+    },
+    bodyText: {
+      maxWidth: '700px',
+    },
+  })
+);
 
-export default Calendar;
+const currentDate = '2018-07-17';
+
+export default function About(): JSX.Element {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Typography variant="h2" component="h1">
+        Calendar
+      </Typography>
+      <Scheduler data={exampleCalendarEvents}>
+        <ViewState currentDate={currentDate} />
+        <MonthView />
+        <Appointments />
+      </Scheduler>
+    </div>
+  );
+}
